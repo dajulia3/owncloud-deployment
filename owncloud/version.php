c774b42d19880329f3a3f19398d83cc1bd7af39b
David Julia <dajulia3@gmail.com>
2016-11-30 13:04:40 -0600

    initial commit -- owncloud v 9.1.2

---
+++ owncloud/version.php
@@ -0,0 +1,7 @@
+<?php 
+$OC_Version = array(9,1,2,5);
+$OC_VersionString = '9.1.2';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_VersionCanBeUpgradedFrom = array(9,0);
+$OC_Build = '2016-11-08T10:54:28+00:00 d47c589b0429eafa5add3a9ffc1c89f8ecccf9cc';
